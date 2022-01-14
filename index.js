30589e5678961d1653e5c769af5a8ee71103951a
Bianca Dias <97123676+BiancaDS06@users.noreply.github.com>
2022-01-14 10:38:31 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+//Objeto que instancia a classe userController
+let useController = new UserController("form-user-create", "form-user-update", "table-users");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*document.querySelectorAll("button").forEach(function(){
+    this.addEventListener("click", function(){
+        console.log("Clicou!");
+    });
+});
+*/
+/*
+var fields= document.querySelectorAll("#form-user-create [name] ");
+var user = {};
+//Não é mais o obejeto json, mas sim um objeto, uma instância da classe. Agora tem user na frente ao ver no console, é a classe a qual esse objeto instanciou.
+function addLine(dataUser){
+
+*/
